1925d76038f256a7203c7f80262a5f9680c43bfe
Marcos Andrade <marcos_q_andrade@hotmail.com>
2023-10-22 20:57:12 -0300

    adicao do mat-card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';  // Importe o componente
+import { HomeComponent } from './components/home/home.component';  // Importe o componente
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 const routes: Routes = [
+  
+  { path: '', component: SidebarComponent, children: [
+  ]},
   { path: 'home', component: HomeComponent },  // Rota padrão para a página inicial
   // Outras rotas, se necessário
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
